19710e44ac49918d70c774b314deec320123fcbf
v-excelsior <vakyla98@gmail.com>
2020-11-21 23:59:10 +0200

    styling header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <header class="header">
-    <div class="header_icon">
-        <img src="assets/logo-36.png" alt="CodeWars logo">
-
+    <div class="header_main">
+        <img class="header_main" src="assets/logo-36.png" alt="CodeWars logo">
+        <span class="header_heading">Codewars | stats</span>
     </div>
     <span class="header_name">v - excelsior</span>
 </header>
@@ -19,9 +19,21 @@
     width: 100%;
     padding: 8px;
     background-color: #222;
+    font-size: 18px;
+
+    &_main{
+      display: flex;
+      align-items: center;
+    }
+
+    &_heading{
+      padding: 0 12px;
+
+      color:#bb432c;
+    }
 
     &_name {
-      color: red;
+      color: #bb432c;
       padding: 0 20px;
     }
   }
