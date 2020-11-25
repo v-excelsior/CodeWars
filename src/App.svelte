d2c57c8a208ee2554cc35526971262b766e00dba
v-excelsior <vakyla98@gmail.com>
2020-11-25 23:45:58 +0200

    research code formating

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,6 @@
 	import Modal from 'svelte-simple-modal';
 	import { Card, Header } from './components'
 
-	import prettier from "https://unpkg.com/prettier/esm/standalone.mjs";
-
 	import cata from './catas/JavaScript/3__Make_a_spiral'
 
 	console.log(cata)
@@ -14,6 +12,9 @@
 		<Modal>
 			<Header/>
 		</Modal>
+		<pre class="x"><code>
+						{cata}
+			</code></pre>
 		<h1 class="xxx">Hello!</h1>
 		<Card/>
 	</main>
@@ -21,6 +22,18 @@
 
 <style lang="scss">
 	@import './src/styles/vars.scss';
+
+	pre, code {
+		font-family: monospace, monospace;
+	}
+	pre {
+		overflow: auto;
+	}
+	pre > code {
+		display: block;
+		padding: 1rem;
+		word-wrap: normal;
+	}
 
 	.app-content {
 		min-height: 100vh;
