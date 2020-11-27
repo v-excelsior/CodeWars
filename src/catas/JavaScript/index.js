f14928063571bc483f5f82a2071de45003e5389e
v-excelsior <vakyla98@gmail.com>
2020-11-27 23:52:17 +0200

    add catas map

---
+++ src/catas/JavaScript/index.js
@@ -0,0 +1,13 @@
+import make_a_spiral from './3_make_a_spiral'
+import permutations from './4_permutations'
+import range_extration from './4_range_extration'
+import the_observed_pin from './4_the_observed_pin'
+import dubstep from './6_dubstep'
+
+export default {
+  make_a_spiral,
+  permutations,
+  range_extration,
+  the_observed_pin,
+  dubstep
+}
