5c80b165d9216398290626e9aa9284c454bc861c
v-excelsior <vakyla98@gmail.com>
2020-11-28 23:55:13 +0200

    refactor cata

--- src/catas/JavaScript/index.js
+++ src/catas/JavaScript/index.js
@@ -1,13 +1,18 @@
 import make_a_spiral from './3_make_a_spiral'
 import permutations from './4_permutations'
 import range_extration from './4_range_extration'
+import strip_comments from './4_strip_comments'
 import the_observed_pin from './4_the_observed_pin'
+import array_diff_hero from './4_array_diff_hero'
+import jo
 import dubstep from './6_dubstep'
 
 export default {
   make_a_spiral,
   permutations,
   range_extration,
+  strip_comments,
   the_observed_pin,
+  array_diff_hero,
   dubstep
 }
