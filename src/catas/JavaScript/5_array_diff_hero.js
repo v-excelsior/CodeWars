8685080b44dda973ffcc586d52b7056a6a6b8d7b
v-excelsior <vakyla98@gmail.com>
2020-11-28 23:47:44 +0200

    refactor cata

--- src/catas/JavaScript/5_array_diff_hero.js
+++ src/catas/JavaScript/5_array_diff_hero.js
@@ -1,4 +1,12 @@
+export default {
+  lang: 'JS',
+  name: '',
+  q   : 5,
+  code
+}
+
+const code = `
 function array_diff_very_fast(a, b) {
   b = new Set(b)
   return a.filter( el = !b.has(el))
-}
+}`
