350c8cda468731a0868eec73aef90e126e6569aa
v-excelsior <vakyla98@gmail.com>
2020-11-28 23:56:08 +0200

    refactor cata

--- src/catas/JavaScript/index.js
+++ src/catas/JavaScript/index.js
@@ -3,8 +3,8 @@ import permutations from './4_permutations'
 import range_extration from './4_range_extration'
 import strip_comments from './4_strip_comments'
 import the_observed_pin from './4_the_observed_pin'
-import array_diff_hero from './4_array_diff_hero'
-import jo
+import array_diff_hero from './5_array_diff_hero'
+import josephus_permutation from './5_josephus_permutation'
 import dubstep from './6_dubstep'
 
 export default {
@@ -14,5 +14,6 @@ export default {
   strip_comments,
   the_observed_pin,
   array_diff_hero,
+  josephus_permutation,
   dubstep
 }
