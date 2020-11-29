08c83d9dce76777d2f9ec86d279316b672a7f4ea
v-excelsior <vakyla98@gmail.com>
2020-11-29 23:59:05 +0200

    refactor cata

--- src/data/catas/7_two_fighters_one_winner.js
+++ src/data/catas/7_two_fighters_one_winner.js
@@ -1,5 +1,17 @@
+const code = `
 function declareWinner(fighter1, fighter2, firstAttacker) {
   const f1 = Math.ceil(fighter1.health / fighter2.damagePerAttack)
   const f2 = Math.ceil(fighter2.health / fighter1.damagePerAttack)
   return f1 < f2 ? fighter2.name : (f2 < f1 ? fighter1.name : firstAttacker)
 }
+}`
+
+export default {
+  lang: 'JS',
+  name: 'Two fighters, one winner',
+  q   : 7,
+  code
+}
+
+
+
