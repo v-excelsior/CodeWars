aff3d755baf0baa8a9d1e2492fc253508feee8b5
v-excelsior <vakyla98@gmail.com>
2020-11-30 20:37:41 +0200

    add cata

---
+++ src/data/catas/4_sum_of_intervals.js
@@ -0,0 +1,23 @@
+const code = `
+function sumIntervals(intervals){
+  intervals.sort((i1,i2) => i1[0] - i2[0])
+  for(let i = 1; i < intervals.length;){
+    if(intervals[i][1] <= intervals[i-1][1]){
+      intervals.splice(i,1)
+      continue
+    }
+    if(intervals[i][0] < intervals[i-1][1]){
+      intervals[i-1][1] = intervals[i][1]
+      intervals.splice(i,1)
+    }
+    i++
+  }
+  return intervals.reduce((acc,el) => acc += el[1] - el[0],0)
+}`
+
+export default {
+    lang:'JS',
+    name: 'Sum of Intervals',
+    q: 4,
+    code
+}
