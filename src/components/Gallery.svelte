ceb5dbe1746358621238606bfe61df4d644c6a7a
v-excelsior <vakyla98@gmail.com>
2020-12-06 20:21:17 +0200

    fix packages

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -1,13 +1,11 @@
 <script>
   import Card from './Card.svelte'
-
   import catas from '../data/catas'
 
-  console.log(catas.forEach(x => console.log(x.lang)))
 </script>
 
 {#each catas as cata}
-    <p>
+    <>
         <Card data={cata}/>
-    </p>
+    </>
 {/each}
